6011363ff588676fc0284a540b23b2b78e01a294
Shiv Kumar Singh <shivsingh1309@gmail.com>
2025-05-23 18:34:55 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
+  server: {
+    proxy: {
       '/api': 'http://localhost:4400'
     }
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
   },
   plugins: [react()],
 })
